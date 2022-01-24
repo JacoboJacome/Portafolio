e88611063697b4fa97d9be3d5fd3e0fb939fff06
JacoboJacome <jacobojacome2@gmail.com>
2022-01-24 14:07:06 -0500

    text darkmode

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -12,7 +12,9 @@ const { darkMode, handleDarkMode } = props
       <div className={`${darkMode ? "bg-cyan-700" : "bg-dark_1"} w-full flex flex-col`}>
         <h2 className={`${darkMode ? "text-color_4" : "text-dark_4"} mx-auto font-bold text-3xl sm:text-4xl font-sans`}>Jacobo Jácome Acosta</h2>
         <h1 className={`${darkMode ? "text-color_4" : "text-dark_4"} mx-auto font-bold text-2xl sm:text-3xl font sans py-2`}>Desarrollador FrontEnd</h1>
-        <button onClick={handleDarkMode} className={`${ darkMode ? "bg-cyan-700 text-color_4 h-10" : "bg-dark_1 text-dark_4 h-10"} sm:absolute mx-auto md:left-20  `}><img src={darkMode? Moon : Sun} rel="noreferrer" alt="Dark Mode"/></button>
+        <div className="flex h-14 justify-center content-center mx-auto md:left-20">
+          <button onClick={handleDarkMode} className={`${ darkMode ? "bg-cyan-700 text-color_4 h-10" : "bg-dark_1 text-dark_4 h-10"} `}><img className="mx-auto" src={darkMode? Moon : Sun} rel="noreferrer" alt="Dark Mode"/><p className="">{darkMode ? "Dark Mode" : "Light Mode"}</p></button>
+        </div>
       </div>
     )
 }
